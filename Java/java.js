4aaad3da26bf164e36a7d6b12fdb75115c10e81c
Georgia <ghobs987@gmail.com>
2021-06-12 00:32:08 +0100

    javascript reveal btn and images added with links to other repos. html formating and some css

---
+++ Java/java.js
@@ -0,0 +1,15 @@
+var gBtn = document.getElementById ("gBtn");
+var gBotPic = document.getElementById ("RoboPic");
+
+gBtn.addEventListener("click",function() {
+    console.log(gBotPic);
+});
+
+
+gBotPic.addEventListener("click",function(event) {
+    
+{event.target.src="./assets/img/georgiaimg.jpg"} 
+    })
+
+
+   
